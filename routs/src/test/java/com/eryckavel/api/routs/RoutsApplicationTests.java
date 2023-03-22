9befaa1a78a625d9a5259271227f1f3e91e46148
eryckavel <eryckavel@gmail.com>
2023-03-22 13:51:46 -0300

    subindo projeto inicial maven

---
+++ routs/src/test/java/com/eryckavel/api/routs/RoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eryckavel.api.routs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
