015cf178d427d983ec674701f2f85f53a512fba3
eryckavel <eryckavel@gmail.com>
2023-03-22 13:44:07 -0300

    subindo projeto inicial

---
+++ routs/src/main/java/com/eryckavel/api/routs/RoutsApplication.java
@@ -0,0 +1,13 @@
+package com.eryckavel.api.routs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutsApplication.class, args);
+	}
+
+}
